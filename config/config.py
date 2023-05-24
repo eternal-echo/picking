a25570b2da70a93e570e67b83548e55c8fa3e9e2
yuanyu <yuanyu@whut.edu.cn>
2023-05-24 11:38:17 +0800

    [track] 编写track接口和例程

---
+++ config/config.py
@@ -0,0 +1,172 @@
+import cv2
+import os
+import json
+import logging
+
+
+class Configurator:
+    def __init__(self, camera, config_dir='config', cache_dir='run'):
+        # camera对象
+        self.camera = camera
+        # 标注工具
+        self.marking = SystemMark()
+        # 运行缓存路径
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+        self.cache_dir = cache_dir
+        # 配置路径
+        if not os.path.exists(config_dir):
+            os.makedirs(config_dir)
+        self.config_dir = config_dir
+        # 初始化日志系统
+        self.__logger = logging.getLogger('config')
+        self.__logger.setLevel(logging.DEBUG)
+        self.__console_handler = logging.StreamHandler()
+        self.__console_handler.setLevel(logging.DEBUG)
+        self.__logger.addHandler(self.__console_handler)
+
+    def get_config(self):
+        # 读取配置文件，获取传送带区域、最大最小零件尺寸信息
+        if os.path.exists(os.path.join(self.config_dir, 'config.json')):
+            with open(os.path.join(self.config_dir, 'config.json'), 'r') as f:
+                config = json.load(f)
+            if 'areas' not in config:
+                self.__logger.error("Failed to get areas")
+                return None
+            else:
+                # 移动相关的参数
+                self.parts_map = config['areas']['parts_map']
+                self.time0 = config['areas']['time0']
+                self.speed = config['areas']['speed']
+                self.spacing = config['areas']['spacing']
+
+            # 未标注传送带区域
+            if 'tracking' not in config or not config['tracking']:
+                # 框选传送带检测范围
+                self.__logger.info("Please select conveyor")
+                self.__wait_for_marking()
+                _, self.bbox_belt = self.marking.mark_belt(self.camera.read()[1])
+
+                # 框选最大零件，用于连通域筛选，去除光线干扰
+                self.__logger.info("Please select the biggest part")
+                self.__wait_for_marking()
+                _, bbox_max = self.marking.mark_part(self.camera.read()[1])
+                self.size_max = (bbox_max[2], bbox_max[3])
+
+                # 框选最小零件，用于连通域筛选，去除传送带噪声
+                self.__logger.info("Please select the smallest part")
+                self.__wait_for_marking()
+                _, bbox_min = self.marking.mark_part(self.camera.read()[1])
+                self.size_min = (bbox_min[2], bbox_min[3])
+
+                # 销毁窗口
+                cv2.destroyAllWindows()
+
+                # 构建JSON对象
+                tracking_config = {
+                    'belt': {
+                        'x': int(self.bbox_belt[0]),
+                        'y': int(self.bbox_belt[1]),
+                        'width': int(self.bbox_belt[2]),
+                        'height': int(self.bbox_belt[3])
+                    },
+                    'max_part': {
+                        'width': int(self.size_max[0]),
+                        'height': int(self.size_max[1])
+                    },
+                    'min_part': {
+                        'width': int(self.size_min[0]),
+                        'height': int(self.size_min[1])
+                    }
+                }
+                config['tracking'] = tracking_config
+                # 保存传送带区域和最大最小零件尺寸的json配置文件
+                with open(os.path.join(self.config_dir, 'config.json'), 'w') as f:
+                    json.dump(config, f, indent=4)
+            # 已标注过传送带区域
+            else:
+                # 跟踪的参数
+                self.bbox_belt = (
+                config['tracking']['belt']['x'], config['tracking']['belt']['y'], config['tracking']['belt']['width'],
+                config['tracking']['belt']['height'])
+                self.size_max = (config['tracking']['max_part']['width'], config['tracking']['max_part']['height'])
+                self.size_min = (config['tracking']['min_part']['width'], config['tracking']['min_part']['height'])
+
+        else:
+            self.__logger.error("Config file not found, please check config/config.json")
+            return None
+
+        return {
+            'bbox_belt': self.bbox_belt,
+            'size_max': self.size_max,
+            'size_min': self.size_min,
+            'parts_map': self.parts_map,
+            'time0': self.time0,
+            'speed': self.speed,
+            'spacing': self.spacing
+        }
+
+        # 等待用户开始标注
+
+    def __wait_for_marking(self):
+        self.__logger.info("Press space to start marking")
+        while True:
+            ret, frame = self.camera.read()
+            if not ret:
+                break
+            cv2.namedWindow("wait for marking", cv2.WINDOW_NORMAL)
+            cv2.imshow("wait for marking", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord(' '):
+                break
+
+
+class SystemMark:
+    def __init__(self):
+        self.conveyor_points = []
+
+    def mark_belt(self, frame: cv2.Mat):
+        """在指定帧中通过两条直线标记出传送带的位置
+
+        Args:
+            frame (cv2.Mat): 传送带所在的帧
+
+        Returns:
+            frame (cv2.Mat): 标记好传送带位置的帧
+            conveyor_lines (list): 两条直线的起点和终点坐标
+        """
+        # 显示原始图像，窗口大小可调整
+        cv2.namedWindow("Mark Conveyor", cv2.WINDOW_NORMAL)
+        cv2.imshow("Mark Conveyor", frame)
+
+        # ROI选择传送带范围
+        rect = cv2.selectROI("Mark Conveyor", frame, False, False)
+        conveyor = frame[int(rect[1]):int(rect[1] + rect[3]), int(rect[0]):int(rect[0] + rect[2])]
+
+        # 销毁窗口
+        cv2.destroyWindow("Mark Conveyor")
+
+        return conveyor, rect
+
+    def mark_part(self, frame: cv2.Mat):
+        """在指定帧中通过ROI框选零件，获取零件的外接矩形
+
+        Args:
+            frame (cv2.Mat): 零件所在的帧
+
+        Returns:
+            part (cv2.Mat): ROI框选的零件图像
+            rect (tuple): 零件的外接矩形坐标
+        """
+        # 显示原始图像
+        cv2.namedWindow("Mark Part", cv2.WINDOW_NORMAL)
+        cv2.imshow("Mark Part", frame)
+
+        # ROI选择零件
+        rect = cv2.selectROI("Mark Part", frame, fromCenter=False, showCrosshair=True)
+        part = frame[int(rect[1]):int(rect[1] + rect[3]), int(rect[0]):int(rect[0] + rect[2])]  # 获取ROI区域的图像
+
+        # 销毁窗口
+        cv2.destroyWindow("Mark Part")
+
+        return part, rect
---
+++ sample/track.py
@@ -0,0 +1,137 @@
+import cv2
+import numpy as np
+import os
+import random
+from config.config import Configurator
+from track_layer.sort.sort import Sort
+
+
+
+class App:
+    def __init__(self, camera_name='1', config_dir='config', cache_dir='run'):
+        if camera_name.isdigit():
+            camera_name = int(camera_name)
+        self.camera = cv2.VideoCapture(camera_name)
+        # 配置
+        self.configurator = Configurator(self.camera, config_dir, cache_dir)
+        self.config_dir = config_dir
+        self.cache_dir = cache_dir
+        # 获取传送带区域和最大最小零件等配置信息
+        config = self.configurator.get_config()
+        self.bbox_belt = config['bbox_belt']
+        self.size_max = config['size_max']
+        self.size_min = config['size_min']
+        self.max_area = self.size_max[0]*self.size_max[1]
+        self.min_area = self.size_min[0]*self.size_min[1]
+
+        # 背景建模
+        self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=50, detectShadows=False)
+        # SORT多目标跟踪
+        self.tracker = Sort(max_age=2, min_hits=3, iou_threshold=0.3)
+
+        self.save_idx = 0
+
+    def start(self):
+        # 打开摄像头
+        if not self.camera.isOpened():
+            print("Failed to open camera")
+            return -1
+        # TODO: 喷嘴控制
+        return 0
+
+    def run(self):
+        while True:
+            ret, frame = self.camera.read()
+            frame_id: int = self.camera.get(cv2.CAP_PROP_POS_FRAMES)
+            if frame is None or not ret:
+                break
+            # [预处理模块]
+            # - 裁剪检测区域
+            belt = frame[int(self.bbox_belt[1]):int(self.bbox_belt[1] + self.bbox_belt[3]),
+                    int(self.bbox_belt[0]):int(self.bbox_belt[0] + self.bbox_belt[2])].copy()
+            # - 中值滤波
+            belt = cv2.medianBlur(belt, 5)
+            # [查找模块]
+            # - 背景建模
+            bg_mask = self.bg_subtractor.apply(belt)
+            pre_proc = cv2.medianBlur(bg_mask, 5)
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+            pre_proc = cv2.morphologyEx(pre_proc, cv2.MORPH_OPEN, kernel)
+            # - 连通域分析
+            origin_nums, origin_labels, origin_stats, origin_centroids = cv2.connectedComponentsWithStats(pre_proc)
+            # 对连通域按面积从大到小排序
+            areas = origin_stats[:, 4]
+            sorted_areas_indices = np.argsort(areas)[::-1]
+            selected_areas_indices = []
+            for i in range(len(sorted_areas_indices)):
+                if (areas[sorted_areas_indices[i]] < self.max_area
+                        # TODO: 边长小于最大尺寸
+                        and max(origin_stats[sorted_areas_indices[i], 2], origin_stats[sorted_areas_indices[i], 3]) < max(self.size_max)
+                        # 区域的边缘处不再选择
+                        and origin_stats[sorted_areas_indices[i], 1] + origin_stats[sorted_areas_indices[i], 3] < pre_proc.shape[0] - 10
+                        and origin_stats[sorted_areas_indices[i], 1] > 10
+                        and origin_stats[sorted_areas_indices[i], 0] > 5
+                        and origin_stats[sorted_areas_indices[i], 0] + origin_stats[sorted_areas_indices[i], 2] < pre_proc.shape[1] - 5):
+                    if areas[sorted_areas_indices[i]] < self.min_area:
+                        break
+                    selected_areas_indices.append(sorted_areas_indices[i])
+            obj_num = len(selected_areas_indices)
+            obj_stats = origin_stats[selected_areas_indices]
+            obj_centroids = origin_centroids[selected_areas_indices]
+            assert obj_num == len(obj_stats) == len(obj_centroids)
+
+            # [跟踪模块]
+            if obj_num == 0:
+                trackers = []
+            else:
+                detections = np.zeros((obj_num, 5))
+                for i in range(obj_num):
+                    detection = [obj_stats[i, 0], obj_stats[i, 1], obj_stats[i, 0] + obj_stats[i, 2],
+                                obj_stats[i, 1] + obj_stats[i, 3], 1]
+                    detections[i, :] = detection
+                trackers = self.tracker.update(detections)
+
+            # [显示]
+            selected_belt = belt.copy()
+            for d in trackers:
+                # 绘制跟踪目标
+                cv2.rectangle(selected_belt, (int(d[0]), int(d[1])), (int(d[2]), int(d[3])), (0, 255, 0), 2)
+                # id
+                cv2.putText(selected_belt, str(int(d[4])), (int(d[0]), int(d[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+                            (0, 255, 0), 2)
+                # # 保存
+                # os.makedirs(os.path.join(self.cache_dir, 'track_layer'), exist_ok=True)
+                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_src.jpg'.format(int(frame_id), int(d[4]))),
+                #             selected_belt)
+                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_bg.jpg'.format(int(frame_id), int(d[4]))),
+                #             bg_mask)
+                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_connected.jpg'.format(int(frame_id), int(d[4]))),
+                #             connected)
+            cv2.namedWindow("Candidates Belt", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("bg_mask", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("pre_proc", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("connected", cv2.WINDOW_NORMAL)
+            cv2.imshow("Candidates Belt", selected_belt)
+            cv2.imshow("bg_mask", bg_mask)
+            cv2.imshow("pre_proc", pre_proc)       
+            connected = np.zeros_like(pre_proc)
+            # 将连通域labels绘制到图像上
+            for i in range(obj_num):
+                color = 255 * i / max((obj_num - 1), 1)
+                connected[origin_labels == selected_areas_indices[i]] = color
+            cv2.imshow("connected", connected)
+                
+
+            keyboard = cv2.waitKey(1)
+            if keyboard == 'q' or keyboard == 27:
+                break
+
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    system = App(camera_name=r'data\test\num.mp4', config_dir='config', cache_dir='run')
+    if system.start() >= 0:
+        system.run()
+    else:
+        print("Failed to start system")
