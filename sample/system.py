10980551cf4e5e6b3250a4497673f2ccb6a3bcc7
yuanyu <yuanyu@whut.edu.cn>
2023-05-24 14:19:39 +0800

    [system] 先yolo检测再跟踪，慢

--- sample/system.py
+++ sample/system.py
@@ -4,6 +4,7 @@ import os
 import random
 from config.config import Configurator
 from track_layer.sort.sort import Sort
+import detect_layer.YoloDetectAPI.yolo_detectAPI as yolo_detectAPI
 
 
 
@@ -28,6 +29,8 @@ class App:
         self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=50, detectShadows=False)
         # SORT多目标跟踪
         self.tracker = Sort(max_age=2, min_hits=3, iou_threshold=0.3)
+        # YOLOv5检测
+        self.yolo_detect = yolo_detectAPI.DetectAPI(weights='num4.pt', conf_thres=0.5, iou_thres=0.5)
 
         self.save_idx = 0
 
@@ -80,45 +83,64 @@ class App:
             obj_centroids = origin_centroids[selected_areas_indices]
             assert obj_num == len(obj_stats) == len(obj_centroids)
 
-            # [跟踪模块]
-            if obj_num == 0:
-                trackers = []
-            else:
-                detections = np.zeros((obj_num, 5))
-                for i in range(obj_num):
-                    detection = [obj_stats[i, 0], obj_stats[i, 1], obj_stats[i, 0] + obj_stats[i, 2],
-                                obj_stats[i, 1] + obj_stats[i, 3], 1]
-                    detections[i, :] = detection
-                trackers = self.tracker.update(detections)
+            trackers = []
+            # 连通域分析后存在候选目标
+            if obj_num > 0:
+            # [检测模块]
+                detect_res, detect_names = self.yolo_detect.detect([belt])
+                # 获取第一章图片的检测结果
+                detect_res = detect_res[0]
+                # detect_res[1]: list
+                if detect_res is not None:
+                    detections = np.zeros((len(detect_res[1]), 5))
+                    for i, res in enumerate(detect_res[1]):
+                        detect_xmin, detect_ymin, detect_xmax, detect_ymax = res[1]
+                        detect_name = res[0]
+                        detect_conf = res[2]
+                        detection = [detect_xmin, detect_ymin, detect_xmax, detect_ymax, detect_conf]
+                        detections[i, :] = detection
+                        # [跟踪模块]
+                        trackers = self.tracker.update(detections)
+
+                # detections = np.zeros((obj_num, 5))
+                # for i in range(obj_num):
+                #     detection = [obj_stats[i, 0], obj_stats[i, 1], obj_stats[i, 0] + obj_stats[i, 2],
+                #                 obj_stats[i, 1] + obj_stats[i, 3], 1]
+                #     detections[i, :] = detection
+                # trackers = self.tracker.update(detections)
 
             # [显示]
             selected_belt = belt.copy()
-            for d in trackers:
-                # 绘制跟踪目标
-                cv2.rectangle(selected_belt, (int(d[0]), int(d[1])), (int(d[2]), int(d[3])), (0, 255, 0), 2)
-                # id
-                cv2.putText(selected_belt, str(int(d[4])), (int(d[0]), int(d[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
-                            (0, 255, 0), 2)
-                # # 保存
-                # os.makedirs(os.path.join(self.cache_dir, 'track_layer'), exist_ok=True)
-                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_src.jpg'.format(int(frame_id), int(d[4]))),
-                #             selected_belt)
-                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_bg.jpg'.format(int(frame_id), int(d[4]))),
-                #             bg_mask)
-                # cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_{}_connected.jpg'.format(int(frame_id), int(d[4]))),
-                #             connected)
+            connected = np.zeros_like(pre_proc)
+            if trackers is not None:
+                for d in trackers:
+                    # 绘制跟踪目标
+                    cv2.rectangle(selected_belt, (int(d[0]), int(d[1])), (int(d[2]), int(d[3])), (0, 255, 0), 2)
+                    # id
+                    cv2.putText(selected_belt, str(int(d[4])), (int(d[0]), int(d[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+                                (0, 255, 0), 2)
+                
+                # 将连通域labels绘制到图像上
+                for i in range(obj_num):
+                    color = 255 * i / max((obj_num - 1), 1)
+                    connected[origin_labels == selected_areas_indices[i]] = color
+
+                # [保存]
+                os.makedirs(os.path.join(self.cache_dir, 'track_layer'), exist_ok=True)
+                cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_src.jpg'.format(int(frame_id))),
+                            selected_belt)
+                cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_bg.jpg'.format(int(frame_id))),
+                            bg_mask)
+                cv2.imwrite(os.path.join(self.cache_dir, 'track_layer', '{}_connected.jpg'.format(int(frame_id))),
+                    connected)
+                
             cv2.namedWindow("Candidates Belt", cv2.WINDOW_NORMAL)
             cv2.namedWindow("bg_mask", cv2.WINDOW_NORMAL)
             cv2.namedWindow("pre_proc", cv2.WINDOW_NORMAL)
-            cv2.namedWindow("connected", cv2.WINDOW_NORMAL)
             cv2.imshow("Candidates Belt", selected_belt)
             cv2.imshow("bg_mask", bg_mask)
             cv2.imshow("pre_proc", pre_proc)       
-            connected = np.zeros_like(pre_proc)
-            # 将连通域labels绘制到图像上
-            for i in range(obj_num):
-                color = 255 * i / max((obj_num - 1), 1)
-                connected[origin_labels == selected_areas_indices[i]] = color
+            cv2.namedWindow("connected", cv2.WINDOW_NORMAL)
             cv2.imshow("connected", connected)
                 
 
